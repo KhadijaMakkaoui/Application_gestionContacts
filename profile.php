b90b03358198bc34dc4a72502898b7445f1ecd2f
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-04-06 09:09:47 +0000

    Integrer la maquette de la page profile Ajouter un titre et un tableau

--- profile.php
+++ profile.php
@@ -1,1 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h2 class="mb-5">Welcome, alex!</h2>
+        <h4>Your profile:</h4>
+
+        <table class="table table-striped">
+            <tbody>
+                <tr>
+                    <th scope="row">Username</th>
+                    <td>alex</td>
+                </tr>
+                <tr>
+                    <th scope="row">Signup date:</th>
+                    <td>Sun,07 Apr 2019 16:11:25</td>
+
+                </tr>
+                <tr>
+                    <th scope="row">Last login:</th>
+                    <td>Mon,08 Mar 2020 16:11:25</td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
