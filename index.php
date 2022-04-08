9c3ea71cb85351347d93a38f459cf285e5502700
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-04-08 09:18:26 +0000

    integration de la maquette du landing page contient un message et un lien dirigant vers la page de connexion

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <div class=" bg-light vh-100 row flex-column justify-content-center align-content-center">
+        <h1>Hello!</h1>
+        <p>
+            <a href="inscription.php"></a>
+            to start creating your contacts list.
+        </p>
+        <p>Already have an account?
+            <a href="connexion.php">Login here</a>
+        </p>
+    </div>
+</body>
+
+</html>
