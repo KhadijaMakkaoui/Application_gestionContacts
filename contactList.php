63681c03bb889ca40b4b4a5576565ec9a5d0a48a
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-04-12 12:06:38 +0000

    Ajouter la balise script pour linker la page contactList avec le fichier script.js

--- contactList.php
+++ contactList.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact List</title>
-    <link rel="stylesheet" href="/bootsrap/bootstrap.min.css">
+    <!-- <link rel="stylesheet" href="/bootsrap/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   
 </head>
 
@@ -58,15 +59,24 @@
         <form action="" class="row my-5">
             <div class="mb-3 col-6">
                 <label for="name" class="form-label">Name</label>
-                <input type="email" class="form-control" id="name" placeholder="Enter name">
+                <input type="text" class="form-control contactList" id="name" placeholder="Enter name">
+            <small class="text-danger" id="errN"></small>
             </div>
             <div class="mb-3 col-6">
                 <label for="phone" class="form-label">Phone</label>
-                <input type="email" class="form-control" id="phone" placeholder="Enter phone">
+                <input type="text" class="form-control contactList" id="phone" placeholder="Enter phone">
+                <small class="text-danger" id="errP"></small>
+            
             </div>
             <div class="mb-3 col-12">
                 <label for="email" class="form-label">Email address</label>
-                <input type="text" inputmode="email" class="form-control" id="email" placeholder="Enter email">
+                <input type="text" inputmode="email" class="form-control contactList" id="email" placeholder="Enter email">
+                <small class="text-danger" id="errE"></small>
+            </div>
+            <div class="mb-3 col-12">
+            <label for="adresse" class="form-label">Address</label>
+                <textarea name=""  cols="" rows="" class="form-control contactList" id="adresse" placeholder="Enter adresse"></textarea>
+                <small class="text-danger" id="errA"></small>
             </div>
             <div class="col-6 mx-auto">
                 <input type="button" value="Save" id="save " class="btn btn-dark mt-3 col-12 ">
@@ -74,6 +84,7 @@
 
         </form>
     </div>
+    <script src="script.js"></script>
 </body>
 
 </html>
