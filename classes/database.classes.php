0547fd80a2d405fd9e1ffc38ab2cb579b217516a
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-04-15 10:51:51 +0000

    Créer la méthode insert() permetant d'inserer une lign dans une table donnée

--- classes/database.classes.php
+++ classes/database.classes.php
@@ -1,14 +1,33 @@
 <?
 class database{
-    private $dbName="contacts_db";
+    private $dbName="contact_db";
     private $host="localhost";
     private $username="root";
     private $password="";
     private $resultat;
-    public $pdo_conn;
+    private $pdo_conn;
     function __construct(){
-        $this->pdo_conn = new PDO("mysql:host=$this->host;dbname=$this->dbName", $this->username, $this->password );
+        try{
+            $this->pdo_conn = new PDO("mysql:host=$this->host;dbname=$this->dbName", $this->username, $this->password );
+        }
+        catch(PDOException $e){
+            echo $e ->getMessage();
+        }
+    }
+    function insert($tableName,$dataArr=array()){
+        //Récupérer 
+        $table_columns = implode(',', array_keys($dataArr));
+        $table_value = implode("','", $dataArr);
+
+        $requete="INSERT INTO $tableName($table_columns) VALUES('$table_value')";
+
+       $resultat = $this->pdo_conn->prepare($requete);
+       $resultat->execute();
+        //Exemple
+        //      insert("utilisateurs",['id_u'=>2,'username'=>"Ahmed"]);
+
     }
    
 }
+
 ?>
