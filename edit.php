a05ea89f929240e1dfcff3ffdecdca397e03c9b8
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-04-19 12:35:28 +0000

    Ajouter le formulaire de modification Afficher les informations du contact selectionné

---
+++ edit.php
@@ -0,0 +1,73 @@
+<?php
+session_start();
+require "./classes/database.classes.php";
+require "./classes/contact.classes.php";
+require "./classes/utilisateur.classes.php";
+?>
+<?php
+//Afficher message d'erreur
+$visibility="d-none";
+if(isset($_GET['error_msg'])){
+    $visibility="";
+}
+$db=new database();
+$get_contact=$db->selectAll("contacts","id='".$_GET['id']."'");
+
+//  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//     $contact=new Contact($_POST['name'],$_POST['email'],$_POST['phone'],$_POST['adresse'],$_SESSION['username']);
+//     $contact->AddContact();
+    
+// }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit contact</title>
+    <!-- <link rel="stylesheet" href="/bootsrap/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container mt-5">
+        <h3>Edit contact:</h3>
+        <div class="alert alert-danger col-11 <?php echo $visibility ?>" role="alert">
+                 <?php echo $_GET['error_msg'] ?>
+                </div>
+        <form action="" method="POST" class="row my-5">
+            <div class="mb-3 col-6">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" value="<?php  echo $get_contact[0]["name"] ?>"class="form-control contactList" name="name" id="name" placeholder="Enter name">
+            <small class="text-danger" id="errN"></small>
+            </div>
+            <div class="mb-3 col-6">
+                <label for="phone" class="form-label">Phone</label>
+                <input type="text" value="<?php  echo $get_contact[0]["phone"] ?>" class="form-control contactList" name="phone"  id="phone" placeholder="Enter phone">
+                <small class="text-danger" id="errP"></small>
+            
+            </div>
+            <div class="mb-3 col-12">
+                <label for="email" class="form-label">Email address</label>
+                <input type="text" value="<?php  echo $get_contact[0]["email"] ?>" inputmode="email" class="form-control contactList" name="email" id="email" placeholder="Enter email">
+                <small class="text-danger" id="errE"></small>
+            </div>
+            <div class="mb-3 col-12">
+            <label for="adresse" class="form-label">Address</label>
+                <textarea cols=""  rows="" class="form-control contactList" name="adresse" id="adresse" placeholder="Enter adresse"><?php  echo $get_contact[0]["adresse"] ?>
+                </textarea>
+                <small class="text-danger" id="errA"></small>
+            </div>
+            <div class="col-6 mx-auto">
+                <input type="submit" value="Save" id="save " class="btn btn-dark mt-3 col-12 ">
+                <button class="btn btn-secondary mt-3 col-12"><a href="contactList.php" class="text-light">Cancel</a></button>
+            
+            </div>
+
+        </form>
+    </div>
+    </div>
+</body>
+</html>
